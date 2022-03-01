ea5e4fe646aead46fb76e8f6c5b94839198db54b
juliek1217 <juliek1217@gmail.com>
2022-03-01 09:13:16 -0500

    symmetric tree

---
+++ Top Interview Questions/Easy/Trees/symmetric_tree.py
@@ -0,0 +1,25 @@
+# Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        queue = collections.deque([root, root])
+        while queue:
+            t1 = queue.pop()
+            t2 = queue.pop()
+            if t1 is None and t2 is None:
+                continue
+            if t1 is None or t2 is None:
+                return False
+            if t1.val != t2.val:
+                return False
+            queue.append(t1.left)
+            queue.append(t2.right)
+            queue.append(t1.right)
+            queue.append(t2.left)
+        return True
