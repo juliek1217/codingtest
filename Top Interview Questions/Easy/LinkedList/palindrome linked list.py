3ad43cad21feb0996454943fe70bc2bf3b1a8e17
juliek1217 <juliek1217@gmail.com>
2022-03-01 01:06:34 -0500

    palindrome linked list

---
+++ Top Interview Questions/Easy/LinkedList/palindrome linked list.py
@@ -0,0 +1,15 @@
+# Given the head of a singly linked list, return true if it is a palindrome.
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        curr = head
+        arr = []
+        while curr is not None:
+            arr.append(curr.val)
+            curr = curr.next
+        return arr == arr[::-1]
